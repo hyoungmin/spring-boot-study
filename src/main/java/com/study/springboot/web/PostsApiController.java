712ff67f0a6beaf7265f0f04c0432826e106e1d9
hyoungmin <gudals123456@gmail.com>
2020-08-14 23:06:55 +0900

    115페이지 업데이트 기능

--- src/main/java/com/study/springboot/web/PostsApiController.java
+++ src/main/java/com/study/springboot/web/PostsApiController.java
@@ -2,11 +2,10 @@ package com.study.springboot.web;
 
 
 import com.study.springboot.service.posts.PostsService;
+import com.study.springboot.web.dto.PostsUpdateRequestDto;
 import com.study.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,5 +16,10 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
     }
 }
