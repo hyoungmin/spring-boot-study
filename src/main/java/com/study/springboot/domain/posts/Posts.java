2ac1870d0edb45dc4f982a5c74602ad45cd0ad3c
hyoungmin <gudals123456@gmail.com>
2020-08-15 22:10:44 +0900

    123페이지 audit column 추가

--- src/main/java/com/study/springboot/domain/posts/Posts.java
+++ src/main/java/com/study/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.study.springboot.domain.posts;
 
+import com.study.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Getter
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
