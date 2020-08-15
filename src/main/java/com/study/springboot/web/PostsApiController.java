e0682c2d8d04460a2d725c160169616c9825d49f
hyoungmin <gudals123456@gmail.com>
2020-08-15 22:03:18 +0900

    118페이지 실제 서버 띄워서 확인하기

--- src/main/java/com/study/springboot/web/PostsApiController.java
+++ src/main/java/com/study/springboot/web/PostsApiController.java
@@ -2,6 +2,7 @@ package com.study.springboot.web;
 
 
 import com.study.springboot.service.posts.PostsService;
+import com.study.springboot.web.dto.PostsResponseDto;
 import com.study.springboot.web.dto.PostsUpdateRequestDto;
 import com.study.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
@@ -21,5 +22,10 @@ public class PostsApiController {
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
