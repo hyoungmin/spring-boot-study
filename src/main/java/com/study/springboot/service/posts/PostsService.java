7747fe6873806531daf8ac48c4a7834c36ebd7d5
hyoungmin <gudals123456@gmail.com>
2020-08-17 22:43:42 +0900

    150페이지 목록 조회하기

--- src/main/java/com/study/springboot/service/posts/PostsService.java
+++ src/main/java/com/study/springboot/service/posts/PostsService.java
@@ -3,12 +3,16 @@ package com.study.springboot.service.posts;
 
 import com.study.springboot.domain.posts.Posts;
 import com.study.springboot.domain.posts.PostsRepository;
+import com.study.springboot.web.dto.PostsListResponseDto;
 import com.study.springboot.web.dto.PostsResponseDto;
 import com.study.springboot.web.dto.PostsSaveRequestDto;
 import com.study.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -29,6 +33,13 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
     public PostsResponseDto findById(Long id) {
